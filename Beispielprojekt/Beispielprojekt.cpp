d5c504a07c4a6222efb5651e9c4a121d029c27d3
Luca Valenziano <Luca Valenziano@DESKTOP-NFJLHE0>
2018-11-06 22:13:38 +0100

    Spieler als struct

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -55,7 +55,6 @@ struct Plattform
 
 	Plattform(Gosu::Image bild, int mitte_x, int mitte_y, int lang = 250, int hoch = 20) : picture(bild),mittelpunkt_x(mitte_x),mittelpunkt_y(mitte_y),laenge(lang),hoehe(hoch)
 	{
-		picture.draw_rot(mittelpunkt_x, mittelpunkt_y, -1.0, 0.0, 0.5, 0.5, 1.0, 1.0);
 	}
 
 };
@@ -64,9 +63,9 @@ struct Plattform
 
 
 
-class Spieler
+struct Spieler
 {
-private:
+//private:
 	//Name des Spielers
 	string name;
 	//Positionen des Spielers
@@ -87,134 +86,49 @@ private:
 
 	vector<Gosu::Image> spielfigur;
 
-	public:
 
-		void set_spielfigur(const Gosu::Image& blick_rechts, const Gosu::Image& blick_links, const Gosu::Image& springen_rechts, const Gosu::Image& springen_links)
-		{
-			this->spielfigur.push_back(blick_rechts);
-			this->spielfigur.push_back(blick_links);
-			this->spielfigur.push_back(springen_rechts);
-			this->spielfigur.push_back(springen_links);
-		}
+	void set_spielfigur(const Gosu::Image& blick_rechts, const Gosu::Image& blick_links, const Gosu::Image& springen_rechts, const Gosu::Image& springen_links)
+	{
+		this->spielfigur.push_back(blick_rechts);
+		this->spielfigur.push_back(blick_links);
+		this->spielfigur.push_back(springen_rechts);
+		this->spielfigur.push_back(springen_links);
+	}
 	
-		void draw()
+	void draw()
+	{
+		if (blick_spieler_rechts && !blick_spieler_links && beschleunigung_spieler_y == 0)
 		{
-			if (blick_spieler_rechts && !blick_spieler_links && beschleunigung_spieler_y == 0)
-			{
-				spielfigur.at(0).draw_rot(position_spieler_x, position_spieler_y, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
-			}
-			else if(blick_spieler_links && !blick_spieler_rechts && beschleunigung_spieler_y == 0)
-			{
-				spielfigur.at(1).draw_rot(position_spieler_x, position_spieler_y, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
-			}
-			else if (blick_spieler_rechts && !blick_spieler_links && beschleunigung_spieler_y != 0)
-			{
-				spielfigur.at(2).draw_rot(position_spieler_x, position_spieler_y, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
-			}
-			else if (blick_spieler_links && !blick_spieler_rechts && beschleunigung_spieler_y != 0)
-			{
-				spielfigur.at(3).draw_rot(position_spieler_x, position_spieler_y, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
-			}
-
+			spielfigur.at(0).draw_rot(position_spieler_x, position_spieler_y, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
+		}
+		else if(blick_spieler_links && !blick_spieler_rechts && beschleunigung_spieler_y == 0)
+		{
+			spielfigur.at(1).draw_rot(position_spieler_x, position_spieler_y, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
+		}
+		else if (blick_spieler_rechts && !blick_spieler_links && beschleunigung_spieler_y != 0)
+		{
+			spielfigur.at(2).draw_rot(position_spieler_x, position_spieler_y, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
+		}
+		else if (blick_spieler_links && !blick_spieler_rechts && beschleunigung_spieler_y != 0)
+		{
+			spielfigur.at(3).draw_rot(position_spieler_x, position_spieler_y, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0);
 		}
 
-		//void plattform_check(const vector<Plattform> plattform_liste)
-		//{
-		//	for(auto it = plattform_liste.begin(); it != plattform_liste.end(); it++)
-		//	if(position_spieler_x > it->links_x && pos )
-		//}
+	}
 
 
-		void set_name(const string& n)
-		{
-			this->name = n;
-		}
-		string get_name()
-		{
-			return this->name;
-		}
-		void set_position_spieler_x(int p)
-		{
-			this->position_spieler_x = p;
-		}
-		int get_position_spieler_x()
-		{
-			return this->position_spieler_x;
-		}
-		void set_position_spieler_y(int p)
-		{
-			this->position_spieler_y = p;
-		}
-		int get_position_spieler_y()
-		{
-			return this->position_spieler_y;
-		}
-		void set_geschwindigkeit_spieler_x(int g)
-		{
-			this->geschwindigkeit_spieler_x = g;
-		}
-		int get_geschwindigkeit_spieler_x()
-		{
-			return this->geschwindigkeit_spieler_x;
-		}
-		void set_geschwindigkeit_spieler_y(int g)
-		{
-			this->geschwindigkeit_spieler_y = g;
-		}
-		int get_geschwindigkeit_spieler_y()
-		{
-			return this->geschwindigkeit_spieler_y;
-		}
-		void set_beschleunigung_spieler_x(int b)
-		{
-			this->beschleunigung_spieler_x = b;
-		}
-		int get_beschleunigung_spieler_x()
-		{
-			return this->beschleunigung_spieler_x;
-		}
-		void set_beschleunigung_spieler_y(int b)
-		{
-			this->beschleunigung_spieler_y = b;
-		}
-		int get_beschleunigung_spieler_y()
-		{
-			return this->beschleunigung_spieler_y;
-		}
-		void set_blick_spieler_rechts(bool b)
-		{
-			this->blick_spieler_rechts = b;
-		}
-		bool get_blick_spieler_rechts()
-		{
-			return this->blick_spieler_rechts;
-		}
-		void set_blick_spieler_links(bool b)
-		{
-			this->blick_spieler_links = b;
-		}
-		bool get_blick_spieler_links()
-		{
-			return this->blick_spieler_links;
-		}
-		void set_leben_spieler(int l)
-		{
-			this->leben_spieler = l;
-		}
-		int get_leben_spieler()
-		{
-			return this->leben_spieler;
-		}
-		void set_lebensanzahl(int l)
-		{
-			this->lebensanzahl = l;
-		}
-		int get_lebensanzahl()
-		{
-			return this->lebensanzahl;
-		}
+	Spieler(int position_x, int position_y, const Gosu::Image& blick_rechts, const Gosu::Image& blick_links,
+		const Gosu::Image& springen_rechts, const Gosu::Image& springen_links)
+		//: position_spieler_x(position_x), position_spieler_y(position_y)
+	{
+		position_spieler_x = position_x;
+		position_spieler_y = position_y;
+		set_spielfigur(blick_rechts, blick_links, springen_rechts, springen_links);
+	}
+	Spieler() 
+	{
 
-		//Spieler(string n, int position_x, int position_y) : name(n), position_spieler_x(position_x), position_spieler_y(position_y) {};
+	};
 };
 
 
@@ -234,10 +148,11 @@ int geschwindigkeit_y(int sprunghoehe)
 int geschwindigkeit_in_y_richtung = geschwindigkeit_y(sprunghoehe);
 
 
-class GameWindow : public Gosu::Window, public Spieler
+class GameWindow : public Gosu::Window
 {
 	Spieler spieler_1;
 	Spieler spieler_2;
+	vector<Plattform> plattformliste;
 
 public:
 	Gosu::Image spieler_bild_1;
@@ -267,21 +182,22 @@ public:
 		hintergrund("Hintergrund_2.jpg"),
 		text_spieler_1(30), text_spieler_2(30), anzeige_leben_spieler_1(30), anzeige_leben_spieler_2(30)
 	{
+
+		//Spieler spieler_1 = Spieler(1, 600, spieler_bild_1, spieler_bild_1_links, spieler_bild_1_springen_rechts, spieler_bild_1_springen_links);
+		//Spieler spieler_2 = Spieler(800, 600, spieler_bild_2, spieler_bild_2_links, spieler_bild_2_springen_rechts, spieler_bild_2_springen_links);
+
+
+		spieler_1.position_spieler_x = 0;
+		spieler_1.position_spieler_y = 600;
 		spieler_1.set_spielfigur(spieler_bild_1, spieler_bild_1_links, spieler_bild_1_springen_rechts, spieler_bild_1_springen_links);
-		spieler_1.set_position_spieler_x(0);
-		spieler_1.set_position_spieler_y(600);
-		spieler_1.set_geschwindigkeit_spieler_y(0);
 
+		spieler_2.position_spieler_x = 800;
+		spieler_2.position_spieler_y = 600;
 		spieler_2.set_spielfigur(spieler_bild_2, spieler_bild_2_links, spieler_bild_2_springen_rechts, spieler_bild_2_springen_links);
-		spieler_2.set_position_spieler_x(800);
-		spieler_2.set_position_spieler_y(600);
-		spieler_2.set_geschwindigkeit_spieler_y(0);
 
-
-		/*vector<Plattform> plattform_liste;
-		plattform_liste.push_back({ 350,450,200,220 });
-		plattform_liste.push_back({ 150,250,200,220 });
-		plattform_liste.push_back({ 150,250,200,220 });*/
+		plattformliste.push_back(Plattform(plattform, 300, 350, 150, 20));
+		plattformliste.push_back(Plattform(plattform, 700, 350, 150, 20));
+		plattformliste.push_back(Plattform(plattform, 500, 150, 150, 20));
 
 
 		//set_caption("Gosu Tutorial Game mit Git");
@@ -293,47 +209,47 @@ public:
 	void bewegung_spieler(Spieler& spieler, Gosu::ButtonName oben, Gosu::ButtonName rechts, Gosu::ButtonName links)
 	{
 
-		spieler.set_position_spieler_x(spieler.get_position_spieler_x() + spieler.get_geschwindigkeit_spieler_x());
-		spieler.set_position_spieler_y(spieler.get_position_spieler_y() + spieler.get_geschwindigkeit_spieler_y());
+		spieler.position_spieler_x = spieler.position_spieler_x + spieler.geschwindigkeit_spieler_x;
+		spieler.position_spieler_y = spieler.position_spieler_y + spieler.geschwindigkeit_spieler_y;
 
 		if (input().down(rechts))					//nach Rechts laufen
 		{
-			spieler.set_geschwindigkeit_spieler_x(5);
-			spieler.set_blick_spieler_rechts(true);
-			spieler.set_blick_spieler_links(false);
+			spieler.geschwindigkeit_spieler_x = 5;
+			spieler.blick_spieler_rechts = true;
+			spieler.blick_spieler_links = false;
 		}
 
 		if (input().down(links))					//nach Links laufen
 		{
-			spieler.set_geschwindigkeit_spieler_x(-5);
-			spieler.set_blick_spieler_rechts(false);
-			spieler.set_blick_spieler_links(true);
+			spieler.geschwindigkeit_spieler_x = -5;
+			spieler.blick_spieler_rechts  = false;
+			spieler.blick_spieler_links  = true;
 		}
 
 		// stehen bleiben, wenn keine Richtung eingegeben wird bzw. wenn beide Richtungen betätigt
 		if ((!input().down(rechts) && !input().down(links)) || (input().down(rechts) && input().down(links)))
 		{
-			spieler.set_geschwindigkeit_spieler_x(0);
+			spieler.geschwindigkeit_spieler_x = 0;
 		}
 
 
-		if (input().down(oben) && (spieler.get_beschleunigung_spieler_y() == 0))					//Springen
+		if (input().down(oben) && (spieler.beschleunigung_spieler_y == 0))					//Springen
 		{
-			spieler.set_geschwindigkeit_spieler_y(-geschwindigkeit_in_y_richtung);
+			spieler.geschwindigkeit_spieler_y = -geschwindigkeit_in_y_richtung;
 		}
-		if (spieler.get_position_spieler_y() >= 600 && !input().down(oben))
+		if (spieler.position_spieler_y >= 600 && !input().down(oben))
 		{
-			spieler.set_geschwindigkeit_spieler_y(0);
-			spieler.set_position_spieler_y(600);
+			spieler.geschwindigkeit_spieler_y = 0;
+			spieler.position_spieler_y = 600;
 		}
-		if (spieler.get_position_spieler_y() < 600)
+		if (spieler.position_spieler_y < 600)
 		{
-			spieler.set_beschleunigung_spieler_y(1);
-			spieler.set_geschwindigkeit_spieler_y(spieler.get_geschwindigkeit_spieler_y() + spieler.get_beschleunigung_spieler_y());
+			spieler.beschleunigung_spieler_y = 1;
+			spieler.geschwindigkeit_spieler_y = spieler.geschwindigkeit_spieler_y + spieler.beschleunigung_spieler_y;
 		}
-		if (spieler.get_position_spieler_y() >= 600)
+		if (spieler.position_spieler_y >= 600)
 		{
-			spieler.set_beschleunigung_spieler_y(0);
+			spieler.beschleunigung_spieler_y = 0;
 		}
 	}
 
@@ -348,33 +264,6 @@ public:
 	int lebensanzahl_spieler_1 = lebensanzahl_anfang;
 	int lebensanzahl_spieler_2 = lebensanzahl_anfang;
 
-	//{
-	//	//Positionen des Spielers
-	//	int position_spieler_1_x = 0;
-	//	int position_spieler_1_y = 600;
-	//	int position_spieler_2_x = 800;
-	//	int position_spieler_2_y = 600;
-
-	//	//Geschwindigkeit des Spielers
-	//	int geschwindigkeit_spieler_1_x;
-	//	int geschwindigkeit_spieler_1_y = 0;
-	//	int geschwindigkeit_spieler_2_x;
-	//	int geschwindigkeit_spieler_2_y = 0;
-
-	//	//Beschleunigung des Spielers
-	//	int beschleunigung_spieler_1_x;
-	//	int beschleunigung_spieler_1_y;
-	//	int beschleunigung_spieler_2_x;
-	//	int beschleunigung_spieler_2_y;
-
-	//	//Blickrichtung des Spielers
-	//	bool blick_spieler_1_rechts = false;
-	//	bool blick_spieler_1_links = false;
-	//	bool blick_spieler_2_rechts = false;
-	//	bool blick_spieler_2_links = false;
-	//}
-
-
 
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
@@ -383,55 +272,28 @@ public:
 	void draw() override
 	{
 
-		Plattform plattform_1(plattform, 300, 350, 150, 20);
-		Plattform plattform_2(plattform, 700, 350, 150, 20);
-		Plattform plattform_3(plattform, 500, 150, 150, 20);
-
 		hintergrund.draw_rot(500, 350, -5.0, 0.0, 0.5, 0.5, 1.25, 1.25);
 
-		//weißer Hintergurnd
-		//graphics().draw_quad
-		//(
-		//	0, 0, Gosu::Color::BLUE,
-		//	800, 0, Gosu::Color::BLUE,
-		//	800, 600, Gosu::Color::BLUE,
-		//	0, 600, Gosu::Color::BLUE,
-		//	-15.0
-		//);
+		for (auto it = plattformliste.begin(); it != plattformliste.end(); it++)
+		{
+			it->draw();
+		}
 
-
-	
 
 		text_spieler_1.draw("Spieler 1", 20, 20, 0.0, 1, 1, Gosu::Color::RED);
 		text_spieler_2.draw(" Spieler 2", 680, 20, 0.0, 1, 1, Gosu::Color::RED);
 		anzeige_leben_spieler_1.draw(to_string(leben_spieler_1), 20, 50, 0.0, 1, 1, Gosu::Color::RED);
 		anzeige_leben_spieler_2.draw(to_string(leben_spieler_2), 680, 50, 0.0, 1, 1, Gosu::Color::RED);
 
-		//spieler_bild_1.draw_rot
-		//(
-		//	position_spieler_1_x,position_spieler_1_y,0.0,1,0.5,1,1.5,1.5
-		//);
-
 
 		spieler_1.draw();
 		spieler_2.draw();
-
-		/*spieler_bild_2.draw_rot
-		(
-			position_spieler_2_x, position_spieler_2_y, 0.0, 1, 0.5, 1, 1.5, 1.5
-		);*/
 	}
 
 
 	// Wird 60x pro Sekunde aufgerufen
 	void update() override
 	{
-
-		/*position_spieler_1_x = position_spieler_1_x + geschwindigkeit_spieler_1_x +beschleunigung_spieler_1_x;
-		position_spieler_1_y = position_spieler_1_y + geschwindigkeit_spieler_1_y;
-
-		position_spieler_2_x = position_spieler_2_x + geschwindigkeit_spieler_2_x + beschleunigung_spieler_2_x;
-		position_spieler_2_y = position_spieler_2_y + geschwindigkeit_spieler_2_y;*/
 
 
 		bewegung_spieler(spieler_1, Gosu::KB_UP, Gosu::KB_RIGHT, Gosu::KB_LEFT);
@@ -497,7 +359,7 @@ public:
 //#########################################################################################
 
 //cout << position_spieler_1_x << "\t" << position_spieler_1_y << "\t" << geschwindigkeit_spieler_1_y << endl;
-		cout << spieler_1.get_position_spieler_x() << endl;
+		cout << spieler_1.position_spieler_x <<"\t" << spieler_1.position_spieler_y << endl;
 	};
 };
 
