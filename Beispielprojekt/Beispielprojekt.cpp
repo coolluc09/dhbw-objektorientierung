f9c2e976cf438548fb51c641ab011a94576c4169
Luca Valenziano <Luca Valenziano@DESKTOP-NFJLHE0>
2018-11-07 19:45:44 +0100

    Auf Plattformen springen funktioniert.

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -41,10 +41,10 @@ struct Plattform
 	int laenge;
 	int hoehe;
 
-	int links_x = mittelpunkt_x - 125;
-	int unten_y = mittelpunkt_y + 10;
-	int rechts_x = mittelpunkt_x - 125;
-	int oben_y = mittelpunkt_y - 10;;
+	int links_x = mittelpunkt_x - (laenge / 2);
+	int rechts_x = mittelpunkt_x + (laenge / 2);
+	int unten_y = mittelpunkt_y + (hoehe / 2);
+	int oben_y = mittelpunkt_y - (hoehe / 2);
 
 
 	Gosu::Image picture;
@@ -63,6 +63,8 @@ struct Plattform
 
 struct Spieler
 {
+	int plattform_nr;
+
 //private:
 	//Name des Spielers
 	string name;
@@ -149,7 +151,7 @@ int geschwindigkeit_in_y_richtung = geschwindigkeit_y(sprunghoehe);
 class GameWindow : public Gosu::Window
 {
 	//bool spiel_fortsetzen = true;
-
+	//int plattform_nr = 3;
 	Spieler spieler_1;
 	Spieler spieler_2;
 	vector<Plattform> plattformliste;
@@ -188,34 +190,66 @@ public:
 		//Spieler spieler_2 = Spieler(800, 600, spieler_bild_2, spieler_bild_2_links, spieler_bild_2_springen_rechts, spieler_bild_2_springen_links);
 
 		//Spieler 1 mit Werten belegen
-		spieler_1.position_spieler_x = 15;
+		spieler_1.position_spieler_x = 235;
 		spieler_1.position_spieler_y = 600;
 		spieler_1.blick_spieler_rechts = true;
 		spieler_1.blick_spieler_links = false;
 		spieler_1.set_spielfigur(spieler_bild_1, spieler_bild_1_links, spieler_bild_1_springen_rechts, spieler_bild_1_springen_links);
 
 		//Spieler 2 mit Werten belegen
-		spieler_2.position_spieler_x = 985;
+		spieler_2.position_spieler_x = 735;
 		spieler_2.position_spieler_y = 600;
 		spieler_2.blick_spieler_rechts = false;
 		spieler_2.blick_spieler_links = true;
 		spieler_2.set_spielfigur(spieler_bild_2, spieler_bild_2_links, spieler_bild_2_springen_rechts, spieler_bild_2_springen_links);
 
 		//Plattformen erstellen
-		plattformliste.push_back(Plattform(plattform, 300, 400, 150, 20));
-		plattformliste.push_back(Plattform(plattform, 700, 400, 150, 20));
-		plattformliste.push_back(Plattform(plattform, 500, 200, 150, 20));
+		plattformliste.push_back(Plattform(plattform, 300, 400, 250, 20));
+		plattformliste.push_back(Plattform(plattform, 700, 400, 250, 20));
+		plattformliste.push_back(Plattform(plattform, 500, 200, 250, 20));
 		plattformliste.push_back(Plattform(hauptplattform, 500, 650, 700, 100));
 
 
 		set_caption("Benjamin und Luca");
 	}
 
+	//bool auf_plattform(vector<Plattform> plattformen, Spieler spieler)
+	//{
+	//	for (int i = 0; i <= 3; i++)
+	//	{
+	//		if (spieler.position_spieler_x >= plattformen.at(i).links_x && spieler.position_spieler_x <= plattformen.at(i).rechts_x)
+	//		{
+	//			if (spieler.position_spieler_y >= (plattformen.at(i).unten_y - 10))
+	//			{
+	//				spieler.plattform_nr = i;
+	//				return true;
+	//			}
+	//		}
+	//	}
+
+	//}
 
 
-
-	void bewegung_spieler(Spieler& spieler, Gosu::ButtonName oben, Gosu::ButtonName rechts, Gosu::ButtonName links)
+	void bewegung_spieler(Spieler& spieler, Gosu::ButtonName oben, Gosu::ButtonName rechts, Gosu::ButtonName links, vector<Plattform> plattformen)
 	{
+		for (int i = 0; i <= 3; i++)
+		{
+
+				if (spieler.position_spieler_y <= (plattformen.at(i).oben_y) && spieler.position_spieler_y >= (plattformen.at(i).oben_y - 20))
+				{
+					if (spieler.position_spieler_x >= plattformen.at(i).links_x && spieler.position_spieler_x <= plattformen.at(i).rechts_x)
+					{
+						spieler.plattform_nr = i;
+						break;
+					}
+					else
+					{
+						spieler.plattform_nr = 4;
+					}
+				}
+		
+		}
+
 		//Position des Spielers aktualisieren
 		spieler.position_spieler_x = spieler.position_spieler_x + spieler.geschwindigkeit_spieler_x;
 		spieler.position_spieler_y = spieler.position_spieler_y + spieler.geschwindigkeit_spieler_y;
@@ -254,25 +288,33 @@ public:
 
 
 		//Springen
-
-		if (input().down(oben) && (spieler.beschleunigung_spieler_y == 0))					
+		if (spieler.plattform_nr <= 3)
 		{
-			spieler.geschwindigkeit_spieler_y = -geschwindigkeit_in_y_richtung;
+			if (input().down(oben) && (spieler.beschleunigung_spieler_y == 0))
+			{
+				spieler.geschwindigkeit_spieler_y = -geschwindigkeit_in_y_richtung;
+			}
+			if (spieler.position_spieler_y >= plattformliste.at(spieler.plattform_nr).oben_y && !input().down(oben))
+			{
+				spieler.geschwindigkeit_spieler_y = 0;
+				spieler.position_spieler_y = plattformliste.at(spieler.plattform_nr).oben_y;
+			}
+			if (spieler.position_spieler_y >= plattformliste.at(spieler.plattform_nr).oben_y)
+			{
+				spieler.beschleunigung_spieler_y = 0;
+			}
+			else
+			{
+				spieler.beschleunigung_spieler_y = 1;
+				spieler.geschwindigkeit_spieler_y = spieler.geschwindigkeit_spieler_y + spieler.beschleunigung_spieler_y;
+			}
 		}
-		if (spieler.position_spieler_y >= 600 && !input().down(oben))
-		{
-			spieler.geschwindigkeit_spieler_y = 0;
-			spieler.position_spieler_y = 600;
-		}
-		if (spieler.position_spieler_y < 600)
+		else
 		{
 			spieler.beschleunigung_spieler_y = 1;
 			spieler.geschwindigkeit_spieler_y = spieler.geschwindigkeit_spieler_y + spieler.beschleunigung_spieler_y;
 		}
-		if (spieler.position_spieler_y >= 600)
-		{
-			spieler.beschleunigung_spieler_y = 0;
-		}
+
 	}
 
 
@@ -322,8 +364,10 @@ public:
 			Window::close();
 		}
 
-		bewegung_spieler(spieler_1, Gosu::KB_UP, Gosu::KB_RIGHT, Gosu::KB_LEFT);
-		bewegung_spieler(spieler_2, Gosu::KB_W, Gosu::KB_D, Gosu::KB_A);
+		//auf_plattform(plattformliste, spieler_1);
+
+		bewegung_spieler(spieler_1, Gosu::KB_UP, Gosu::KB_RIGHT, Gosu::KB_LEFT, plattformliste);
+		bewegung_spieler(spieler_2, Gosu::KB_W, Gosu::KB_D, Gosu::KB_A, plattformliste);
 
 
 		// Abstandsmessung der zwei Spieler
